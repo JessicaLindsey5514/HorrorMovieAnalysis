4e60967b98b8f51fe458a734f1627369244d9938
Jessica <jessicalindsey5514@gmail.com>
2026-05-03 17:53:12 -0400

    cleaner & first plots

---
+++ notebooks/questions_to_explore.py
@@ -0,0 +1,30 @@
+    
+# ---  ------             --------------  -----         
+#  0   id                 31142 non-null  int64         
+#  1   original_title     31142 non-null  str           
+#  2   title              31142 non-null  str           
+#  3   original_language  31142 non-null  str           
+#  4   overview           31142 non-null  str           
+#  5   tagline            12628 non-null  str           
+#  6   release_date       31142 non-null  datetime64[us]
+#  7   popularity         31142 non-null  float64       
+#  8   vote_count         31142 non-null  int64         
+#  9   vote_average       31142 non-null  float64       
+#  10  budget             31142 non-null  int64         
+#  11  revenue            31142 non-null  int64         
+#  12  runtime            31142 non-null  int64         
+#  13  status             31142 non-null  str           
+#  14  genre_names        31142 non-null  str           
+#  15  collection_name    2281 non-null   str         
+
+#PIE total movies that are part of a collection vs. total movies that arent
+#how likely is a movie good enough to become a collection ,
+#have to factor in revenue and popularity in as well 
+#is there a subgenre that typically is more likely to turn into a collection?
+
+#look at "comedy, horror" release dates 
+# look at "horror, science fiction" release dates
+# are socio/economic/political issues that cause trends in movie genres
+# ex. patriotic and war movies after 9/11
+# ex. manic pixie dream girl era
+# ex. how tech is viewed in sci fi movies before and after AI 
